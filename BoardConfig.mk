6b6bc8e22bda3bb154f8c69b27c0fe7c0d15ac2e
EndCredits <alicization.han@gmail.com>
2022-04-01 21:58:24 +0800

    picasso: Remove entire touchfeture
    Change-Id: I23d97b25f81bcbe24d78e20f522a187f1f4c1359

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,7 +84,7 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
