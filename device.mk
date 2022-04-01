d6b4a9ab304672673287799c114f10259dbac535
EndCredits <alicization.han@gmail.com>
2022-04-01 10:30:27 +0800

    picasso: Ready for AOSPA

--- device.mk
+++ device.mk
@@ -22,7 +22,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/picasso-firmware/firmware.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aospa
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
