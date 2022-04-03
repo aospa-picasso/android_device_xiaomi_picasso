ff803ca58e465007fe1b9d7eab7ede4c3fa04e3b
EndCredits <alicization.han@gmail.com>
2022-04-03 12:35:19 +0800

    picasso: vintf: Divide xiaomi specific HALs to framework compatibility matrix
    Change-Id: I212b76a9c639c538cc591e0b81b7280960102243

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,8 +83,13 @@ TARGET_ENABLE_MEDIADRM_64 := true
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
-DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/xiaomi_vendor_framework_compatibility_matrix.xml
+
+DEVICE_MANIFEST_FILE := \
+    $(DEVICE_PATH)/manifest.xml \
+    $(DEVICE_PATH)/xiaomi_manifest.xml
 
 DEVICE_MATRIX_FILE += device/qcom/common/compatibility_matrix.xml
 
