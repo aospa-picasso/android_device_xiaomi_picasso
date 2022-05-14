8bc0be09dbb3aa6cb7c43fe49933946a2128aa63
EndCredits <alicization.han@gmail.com>
2022-05-14 22:52:57 +0800

    picasso: Also copy AVB permission file

--- device.mk
+++ device.mk
@@ -418,6 +418,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# Verified Boot
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
+
 # WFD
 PRODUCT_PACKAGES += \
     libwfdaac_vendor
